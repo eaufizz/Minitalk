2c039207ea812d3dbcc14f29cade54678622e5d8
Shinohara Reo <sreo@c3r4s2.42tokyo.jp>
2024-07-14 19:04:52 +0900

    gannbareo

--- server.c
+++ server.c
@@ -6,13 +6,25 @@
 /*   By: sreo <sreo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 13:56:48 by sreo              #+#    #+#             */
-/*   Updated: 2024/07/14 17:28:38 by sreo             ###   ########.fr       */
+/*   Updated: 2024/07/14 19:03:50 by sreo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
+void print_message(int sig)
+{
+    
+}
 
 int main(void){
-    ft_printf("Server PID %d\n", getpid());
+    int id;
+    
+    id = getpid();
+    ft_printf("Server PID %d\n", id);
+    signal(SIGUSR1, print_message());
+    signal(SIGUSR2, print_message());
+    
+    while(1)
+        pause();
     return 0;
 }
