a781f8f9db8d10524d783a65de5f0a523bcec021
Shinohara Reo <sreo@c3r4s7.42tokyo.jp>
2024-07-20 17:25:27 +0900

    1stSubmit

--- client.c
+++ client.c
@@ -1,30 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sreo <sreo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 15:37:00 by sreo              #+#    #+#             */
+/*   Updated: 2024/07/20 17:24:01 by sreo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf/ft_printf.h"
 #include <signal.h>
 
-void send_message(int pid, char *str)
+int	send_message(int pid, char *str)
 {
-    int i;
-    unsigned int j;
+	int				i;
+	unsigned int	j;
+	int				k;
 
-    j = 0;
-    while(str[j])
-    {
-        i = 8;
-        while(--i >= 0)
-        {
-            if ((str[j] >> i) & 1)
-                kill(pid, SIGUSR2);
-            else
-                kill(pid, SIGUSR1);
-            usleep(100);
-        }
-        j++;
-    }
+	j = 0;
+	while (str[j])
+	{
+		i = 8;
+		while (--i >= 0)
+		{
+			if ((str[j] >> i) & 1)
+				k = kill(pid, SIGUSR2);
+			else
+				k = kill(pid, SIGUSR1);
+			usleep(400);
+			if (k == -1)
+				return (1);
+		}
+		j++;
+	}
+	return (0);
 }
-int main (int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
-    if(argc != 3)
-        return 1;
-    send_message(ft_atoi(argv[1]), argv[2]);
-    return 0;
+	if (argc != 3)
+	{
+		ft_printf("error\n");
+		return (1);
+	}
+	if (send_message(ft_atoi(argv[1]), argv[2]))
+	{
+		ft_printf("error\n");
+		return (1);
+	}
+	ft_printf("success\n");
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,39 +6,40 @@
 /*   By: sreo <sreo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 13:56:48 by sreo              #+#    #+#             */
-/*   Updated: 2024/07/16 20:42:27 by sreo             ###   ########.fr       */
+/*   Updated: 2024/07/20 17:14:08 by sreo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf/ft_printf.h"
 #include <signal.h>
 
-static void print_message(int sig)
+static void	print_message(int sig)
 {
-    static int bit_count = 0;
-    static unsigned char bit_letter = 0;
+	static int				bit_count = 0;
+	static unsigned char	bit_letter = 0;
 
-    if(sig == SIGUSR2)
-        bit_letter |= 1;
-    bit_count ++;
-    if(bit_count == 8)
-    {
-        write(1, &bit_letter, 1);
-        bit_count = 0;
-        bit_letter = 0;
-    }
-    else
-        bit_letter <<= 1;
+	if (sig == SIGUSR2)
+		bit_letter |= 1;
+	bit_count ++;
+	if (bit_count == 8)
+	{
+		write(1, &bit_letter, 1);
+		bit_count = 0;
+		bit_letter = 0;
+	}
+	else
+		bit_letter <<= 1;
 }
 
-int main(void){
-    int id;
-    
-    id = getpid();
-    ft_printf("Server PID %d\n", id);
-    signal(SIGUSR1, print_message);
-    signal(SIGUSR2, print_message);
-    while(1)
-        pause();
-    return 0;
+int	main(void)
+{
+	int	id;
+
+	id = getpid();
+	ft_printf("Server PID %d\n", id);
+	signal(SIGUSR1, print_message);
+	signal(SIGUSR2, print_message);
+	while (1)
+		pause();
+	return (0);
 }
